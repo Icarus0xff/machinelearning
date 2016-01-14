d401837c177eb243ebcce757cd379be59f685a54
augustus <hikkikuma1991@163.com>
2016-01-14 02:04:44 -0800

    test

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -19,7 +19,7 @@ grad = zeros(size(theta));
 
 h_theta = sigmoid(X * theta);
 J = 1/m * sum((-y .* log(h_theta) - (1 .- y) .* log(1 .- h_theta))) + (lambda/(2*m)) * sum(theta(2:end).^2);
-grad = 1024;
+grad = 102;
 % =============================================================
 
 end
